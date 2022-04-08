035e20463f53387cd35da373459a897f90217e6d
Anthony M <anthony.moffa@thoughtbot.com>
2022-04-08 11:09:24 -0400

    Refactor flashcard/card for better readability
    This commit contains a refactor of the card component to make the code a
    bit clearer, more readable. I extracted these changes from the modal
    related changes I made in the other PR
    
    Generally, I changed things based on the following guidelines:
    1. Move inline styles to a `styles` object at the bottom of the file if
       there are more than one or two attributes being added. Apply the
       style to the desired component via `styles.whateverYouNamedIt`
    1. Define variables, functions, hooks, etc., grouped together by their
       function (e.g., hooks together, where possible) and by what they're
       related to, (e.g., style related variables)
    1. Grouping `return`s together where possible
    
    Totally open to discussing the changes, these changes seemed like easy
    and made it easier to work in the file while I was making other changes.
    
    Co-authored-by: Anthony M. <anthony.moffa@hey.com>

--- src/modules/flashcard/card.js
+++ src/modules/flashcard/card.js
@@ -11,28 +11,17 @@ import {isRegistered} from '../../course/course';
 
 const Card = ({
   index,
-  answerCallback,
   revealCallback,
   isActive,
   wasActive,
   isRevealed,
   children,
-  currentCard,
-  ...otherProps
+  currentCard
 }) => {
-  const _children = Children.toArray(children);
-
-  const cardVariants = {
-    initial: {y: 10 * (index - currentCard), opacity: 1},
-    active: {y: 0, opacity: 1},
-    exit: {y: -64, opacity: 0, scale: 1.1}
-  };
-
   const [{data, error}, connect] = useConnect();
   const [{data: accountData}] = useAccount();
-  const [isUserRegistered, setIsUserRegistered] = React.useState(false);
   const provider = useProvider();
-
+  const [isUserRegistered, setIsUserRegistered] = React.useState(false);
   const [isModalVisible, setModalVisible] = React.useState(false);
 
   useEffect(() => {
@@ -44,37 +33,29 @@ const Card = ({
     }
   }, [accountData?.address, provider]);
 
-  if (_children.length < 2) {
-    return (
-      <Flex
-        sx={{
-          width: '343px',
-          height: '439px',
-          borderRadius: 7,
-          bg: 'error',
-          color: 'onBackgroundAlt',
-          flexDirection: 'column',
-          overflow: 'hidden',
-          position: 'relative',
-          zIndex: 1,
-          boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
-        }}
-      >
-        <Flex
-          sx={{
-            p: 3,
-            fontSize: 5,
-            alignItems: 'center',
-            textAlign: 'center',
-            justifyContent: 'center',
-            flex: '1 1 auto'
-          }}
-        >
-          ERROR! Incorrect # of Children for Card. Please check your mdx.
-        </Flex>
-      </Flex>
-    );
-  }
+  const handleOnClickMetamask = () => {
+    connect(data.connectors[Connector.INJECTED]).then((result) => {
+      if (!result.error) {
+        setModalVisible(true);
+      }
+    });
+  };
+
+  const currentVariant = isActive ? 'active' : wasActive ? 'exit' : 'initial';
+  const inactiveScale = 1 - 0.05 * (index - currentCard);
+
+  const cardOuterStyle = {
+    ...styles.cardOuterContainer,
+    display: index - currentCard > 2 ? 'none' : 'flex',
+    opacity: inactiveScale,
+    transform: `scale(${isActive ? '1' : inactiveScale})`
+  };
+
+  const answerTextStyle = {
+    ...styles.answerText,
+    opacity: isRevealed ? 0.8 : 1,
+    filter: isRevealed ? 'blur(0px)' : 'blur(4px)'
+  };
 
   const revealCopyVariant = {
     revealed: {opacity: 0, y: 12, transition: {duration: 0.2}},
@@ -91,21 +72,26 @@ const Card = ({
     initial: {opacity: 0}
   };
 
-  const question = _children[0];
-
-  const answer = _children[1];
-
-  const postAnswer = _children.slice(2, _children.length);
-  const currentVariant = isActive ? 'active' : wasActive ? 'exit' : 'initial';
-  const inactiveScale = 1 - 0.05 * (index - currentCard);
-
-  const handleOnClickMetamask = () => {
-    connect(data.connectors[Connector.INJECTED]).then((result) => {
-      if (!result.error) {
-        setModalVisible(true);
-      }
-    });
+  const cardVariants = {
+    initial: {y: 10 * (index - currentCard), opacity: 1},
+    active: {y: 0, opacity: 1},
+    exit: {y: -64, opacity: 0, scale: 1.1}
   };
+
+  const _children = Children.toArray(children);
+  const question = _children[0];
+  const answer = _children[1];
+  const postAnswer = _children.slice(2, _children.length);
+
+  if (_children.length < 2) {
+    return (
+      <Flex sx={styles.errorContainer}>
+        <Flex sx={styles.errorText}>
+          ERROR! Incorrect # of Children for Card. Please check your mdx.
+        </Flex>
+      </Flex>
+    );
+  }
 
   return (
     <motion.div variants={cardVariants} animate={currentVariant}>
@@ -113,66 +99,12 @@ const Card = ({
         setModalVisible={setModalVisible}
         isModalVisible={isModalVisible}
       />
-      <Flex
-        sx={{
-          width: ['100%', '343px', '343px'],
-          height: ['58vh', '439px', '439px'],
-          borderRadius: 7,
-          bg: 'backgroundAlt',
-          color: 'onBackgroundAlt',
-          display: index - currentCard > 2 ? 'none' : 'flex',
-          opacity: inactiveScale,
-          flexDirection: 'column',
-          overflow: 'hidden',
-          transform: `scale(${isActive ? '1' : inactiveScale})`,
-          transformOrigin: 'bottom',
-          boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
-        }}
-      >
+      <Flex sx={cardOuterStyle}>
         {isActive && (
           <>
-            <Flex
-              sx={{
-                alignItems: 'center',
-                textAlign: 'center',
-                justifyContent: 'center',
-                flex: '1 1 auto',
-                p: [1, 2, 3],
-                fontSize: [2, 3, 4]
-              }}
-            >
-              {question}
-            </Flex>
-            <Flex
-              sx={{
-                height: '38%',
-                bg: 'primaryMuted',
-                borderTop: '1px solid',
-                borderColor: 'background',
-                position: 'relative',
-                color: 'text',
-                px: 2,
-                pt: '20px',
-                overflow: 'hidden',
-                alignItems: 'center',
-                justifyContent: 'center',
-                cursor: 'pointer',
-                flexDirection: 'column',
-                '&:hover .reveal-answer': {
-                  transition: 'all .2s ease',
-                  transform: 'scale(1.1)'
-                }
-              }}
-            >
-              <div
-                sx={{
-                  position: 'absolute',
-                  boxShadow: '0px 0 10px rgba(0,0,0,0.3)',
-                  top: '-13px',
-                  height: '13px',
-                  width: '100%'
-                }}
-              ></div>
+            <Flex sx={styles.questionContainer}>{question}</Flex>
+            <Flex sx={styles.answerContainer}>
+              <div sx={styles.answerContainerShadow} />
               <motion.div
                 variants={revealCopyVariant}
                 initial="initial"
@@ -183,102 +115,52 @@ const Card = ({
                   <Flex onClick={revealCallback}>
                     <span
                       className="reveal-answer"
-                      sx={{
-                        fontSize: [3, 4, 4],
-                        mb: 2,
-                        fontWeight: 'bold',
-                        transform: 'scale(1)',
-                        transition: 'all .2s ease'
-                      }}
+                      sx={styles.revealAnswerText}
                     >
-                      {' '}
                       Reveal the Answer
                     </span>
                   </Flex>
                 )}
                 {!data.connected && (
-                  <>
-                    <div>
-                      <>
-                        <Box
-                          sx={{
-                            padding: '0.5rem'
-                          }}
-                        >
-                          <Text
-                            sx={{
-                              textAlign: 'center',
-                              fontWeight: 'bold',
-                              marginX: 'auto'
-                            }}
-                          >
-                            Connect wallet to reveal
-                          </Text>
-                        </Box>
-
-                        <Button
-                          sx={{marginX: 'auto'}}
-                          disabled={!data.connectors[Connector.INJECTED].ready}
-                          onClick={handleOnClickMetamask}
-                        >
-                          Metamask
-                        </Button>
-                      </>
-                      {error && (
-                        <div>{error?.message ?? 'Failed to connect'}</div>
-                      )}
-                    </div>
-                  </>
+                  <div>
+                    <Box sx={{padding: '0.5rem'}}>
+                      <Text sx={styles.promptText}>
+                        Connect wallet to reveal
+                      </Text>
+                    </Box>
+                    <Button
+                      sx={{marginX: 'auto'}}
+                      disabled={!data.connectors[Connector.INJECTED].ready}
+                      onClick={handleOnClickMetamask}
+                    >
+                      Metamask
+                    </Button>
+                    {error && (
+                      <div>{error?.message ?? 'Failed to connect'}</div>
+                    )}
+                  </div>
                 )}
               </motion.div>
               {data.connected && !isUserRegistered && (
-                <>
-                  <div>
-                    <>
-                      <Box
-                        sx={{
-                          padding: '0.5rem'
-                        }}
-                      >
-                        <Text
-                          sx={{
-                            textAlign: 'center',
-                            fontWeight: 'bold',
-                            marginX: 'auto'
-                          }}
-                        >
-                          Register to reveal
-                        </Text>
-                      </Box>
-
-                      <Button
-                        sx={{marginX: 'auto'}}
-                        disabled={!data.connectors[Connector.INJECTED].ready}
-                        onClick={() => setModalVisible(true)}
-                      >
-                        Register
-                      </Button>
-                    </>
-                  </div>
-                </>
+                <div>
+                  <Box sx={{padding: '0.5rem'}}>
+                    <Text sx={styles.promptText}>Register to reveal</Text>
+                  </Box>
+                  <Button
+                    sx={{marginX: 'auto'}}
+                    disabled={!data.connectors[Connector.INJECTED].ready}
+                    onClick={() => setModalVisible(true)}
+                  >
+                    Register
+                  </Button>
+                </div>
               )}
-              {/* Reveal answer when Wallet is Connected and User is Registered */}
               {data.connected && isUserRegistered && (
                 <motion.div
                   variants={answerCopyVariant}
                   initial="initial"
                   animate={isRevealed ? 'revealed' : 'initial'}
-                  sx={{
-                    overflow: 'auto',
-                    '& > *:first-child': {
-                      fontSize: [3, 4, 4],
-                      textAlign: 'center',
-                      fontWeight: 'bold',
-                      opacity: isRevealed ? 0.8 : 1,
-                      filter: isRevealed ? 'blur(0px)' : 'blur(4px)',
-                      transition: 'all .2s ease'
-                    }
-                  }}
+                  sx={answerTextStyle}
                 >
                   {answer}
                   {_children.length > 2 && (
@@ -299,6 +181,95 @@ const Card = ({
       </Flex>
     </motion.div>
   );
+};
+
+const styles = {
+  answerContainer: {
+    height: '38%',
+    bg: 'primaryMuted',
+    borderTop: '1px solid',
+    borderColor: 'background',
+    position: 'relative',
+    color: 'text',
+    px: 2,
+    pt: '20px',
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+    cursor: 'pointer',
+    flexDirection: 'column',
+    '&:hover .reveal-answer': {
+      transition: 'all .2s ease',
+      transform: 'scale(1.1)'
+    }
+  },
+  answerContainerShadow: {
+    position: 'absolute',
+    boxShadow: '0px 0 10px rgba(0,0,0,0.3)',
+    top: '-13px',
+    height: '13px',
+    width: '100%'
+  },
+  answerText: {
+    overflow: 'auto',
+    '& > *:first-child': {
+      fontSize: [3, 4, 4],
+      textAlign: 'center',
+      fontWeight: 'bold',
+      transition: 'all .2s ease'
+    }
+  },
+  cardOuterContainer: {
+    width: ['100%', '343px', '343px'],
+    height: ['58vh', '439px', '439px'],
+    borderRadius: 7,
+    bg: 'backgroundAlt',
+    color: 'onBackgroundAlt',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    transformOrigin: 'bottom',
+    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
+  },
+  errorContainer: {
+    width: '343px',
+    height: '439px',
+    borderRadius: 7,
+    bg: 'error',
+    color: 'onBackgroundAlt',
+    flexDirection: 'column',
+    overflow: 'hidden',
+    position: 'relative',
+    zIndex: 1,
+    boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)'
+  },
+  errorText: {
+    p: 3,
+    fontSize: 5,
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    flex: '1 1 auto'
+  },
+  promptText: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginX: 'auto'
+  },
+  questionContainer: {
+    alignItems: 'center',
+    textAlign: 'center',
+    justifyContent: 'center',
+    flex: '1 1 auto',
+    p: [1, 2, 3],
+    fontSize: [2, 3, 4]
+  },
+  revealAnswerText: {
+    fontSize: [3, 4, 4],
+    mb: 2,
+    fontWeight: 'bold',
+    transform: 'scale(1)',
+    transition: 'all .2s ease'
+  }
 };
 
 export default Card;
